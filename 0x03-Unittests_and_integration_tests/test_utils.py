d7a5d3fa1f16b1860c9babed9a8d4e825c1b7045
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-05-21 19:13:07 +0100

    [feat]: Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ Unittesting for Utils """
 import unittest
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from parameterized import parameterized, parameterized_class
-from pytest import raises
+from unittest.mock import patch, MagicMock
+
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -26,3 +27,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Test the nested map exceptions """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+    
+
+class TestGetJson(unittest.TestCase):
+    """ Test for get_json """
+
+    @parameterized.expand([
+        ("http://example.com",{"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """ testing utils.get_json function """
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+
